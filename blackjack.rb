2ffa1ed17fd4b8dc4f2b79856deed0b98b3b5414
Jamal Khan <me@jamalkhan.com>
2011-12-20 22:40:14 +0300

    Blackjack fully implmented with double functionality

--- blackjack.rb
+++ blackjack.rb
@@ -3,17 +3,18 @@ SUITE = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 class Player
   attr_accessor :cards, :amount , :is_playing, :bet
   
-  def initialize(amount, cards, is_dealer, player_number)
-    
+  def initialize(amount, cards, player_number)
     @amount = amount
     @cards = cards
     @is_playing = true
     @bet = 0
-    @is_dealer = is_dealer
-    @player_number = player_number
-    
+    @player_number = player_number    
   end
   
+  def blackjack()
+    return (@cards.length == 2 and value() == 21)
+  end
+    
   def print_hand()
     puts "Hand: #{@cards} Value: #{value()}"
   end
@@ -46,33 +47,69 @@ class Player
   
 end
 
-
 class Blackjack 
-
-  attr_accessor :players
   
   def initialize()
     
-    @players =  {}#Array.new # Players
+    @players =  {} # Players array
+    @num_players = 0
     @num_decks = 4 # Number of card decks
     @cards = Array.new # Universal cards that are held with the dealer
-    
-    @dealer = Player.new(0, Array.new, true, -1)
+    @dealer = Player.new(0, Array.new, -1) # Dealer
     
     init_game()
-    game()
+    play_game()
 
   end
   
-  def game()
-    
+  def play_game()
+    # game will continue until people have run out of money
     while (true)
       init_round()
       round()
     end
+  end
+  
+  def init_round()
+    
+    # Initializing the card deck
+    deck_size = @num_decks * 4
+    deck_size.times {@cards += SUITE}
+    
+    @players.each do |k, p|
+      p.reset()
+    end
+    
+    @dealer.cards = [get_card, get_card]
+    
+    # Check if players are bust
+    @players.delete_if{|k, p| p.amount <= 0}
+    
+    if @players.size == 0
+      puts "Game Ended"
+      exit()
+    end
+   
+    puts "============================="
+    puts "|         NEW ROUND          |"
+    puts "============================="
+    
+    # give each player 2 initial cards and get their bets
+    @players.each do |k, p| 
+
+      p.cards = [get_card, get_card]
+      
+      while (p.bet <=0 or p.bet > p.amount)
+        puts "Player #{k}, please enter your bet, a number less than or equal to #{p.amount}"
+        p.bet = gets.to_i
+      end
+      
+      
+    end
+    
+    print_game()
     
   end
-    
   
   def round()
     
@@ -80,94 +117,121 @@ class Blackjack
       puts "## PLAYER #{k} ##"
       while p.is_playing
         
-        if p.value() == 21 and p.cards.length() == 2
-          puts "Blackjack was acheived"
+        if p.blackjack()
+          puts "Blackjack was acheived!"
+          p.print_hand
+          break
+        elsif p.value == 21
+          puts "21 was acheived!"
           p.print_hand
           break
         elsif p.value() > 21
-          puts "You lost"
           puts p.print_hand
+          puts "You got bust!"
           break
         end
         
         p.print_hand() 
-        print "Please choose from the following {hit, stay, split}"
+        puts "Please choose from the following {hit, stay, split, double}:"
         decision = gets.chomp
         
-        case decision
-        
-        when "hit"
+        if decision == "hit"
           p.cards.push(get_card)
-          
-        when "stand"
+        elsif decision ==  "stay"
           p.is_playing = false
-
-        when "split"
-          puts "Not implemented dudu"
-          
+        elsif decision == "split"
+          puts "Not implemented split functionality"
+        elsif decision == "double"
+          p.cards.push(get_card)
+          p.is_playing = false
+          p.print_hand
+          puts "double was called"
         end
       end
     end
     
-    end_game()
+    end_round()
   end
   
   def print_game()
     
     puts "============================="
+    puts "|         GAME STATE         |"
+    puts "============================="
+    puts "|        DEALER CARD  |#{@dealer.cards[0]}|    |"
+    puts "============================="
+        
+    @players.each do |k, p|
+       puts "|        PLAYER: #{k}   #{p.cards[0]}, #{p.cards[1]}    |"
+       puts "============================="
+    end
+    
+  end
+  
+  def end_round()
+    
+    puts "We reached the end of the round"
+    
+    # Reveal dealers last card
+    while @dealer.value() < 17
+      @dealer.cards.push(get_card)
+    end
+    puts "The dealer got the following hand:"
+    @dealer.print_hand()
+    
+    dealer_value = @dealer.value()
+    
+    if dealer_value > 21
+      #blackjack case
+      puts "The dealer lost."
+      @players.each do |k, p|
+        if p.blackjack()
+          p.amount += (p.bet * 1.5)
+          puts "Player #{k} got blackjack and has #{p.amount} left in their account"
+        else
+          p.amount += p.bet
+          puts "Player #{k} won and has #{p.amount} left in their account"
+        end
+      end
+    else
+      @players.each do |k, p|
+        if p.blackjack()
+          p.amount += (p.bet * 1.5)
+          puts "Player #{k} got blackjack and has #{p.amount} left in their account"
+        elsif p.value() > dealer_value and p.value() <= 21
+          p.amount += p.bet
+          puts "Player #{k} won and has #{p.amount} left in their account"
+        elsif p.value() == dealer_value
+          puts "Player #{k} drawn and has #{p.amount} left in their account"
+        else
+          p.amount -= p.bet
+          puts "Player #{k} lost and has #{p.amount} left in their account"
+        end
+        
+      end
+    end
+    
+  end
+
+  def init_game()
+    
+    puts "============================="
     puts "|         BLACKJACK          |"
     puts "============================="
     
-    puts "DEALER CARD  |#{@dealer.cards[0]}|"
-    
-    @players.each do |k, p|
-       puts "PLAYER #{k}: #{p.cards[0]}, #{p.cards[1]} "
-      
-    end
-    
-  end
-  
-  def end_game()
-    
-    # Reveal dealers last card
-    # Determine who who won and do the debiting
-    
-  end
-  
-  def game_loop()
-    
-    @dealer.cards = [get_card, get_card]
-    
-    #Give each player 2 initial cards and get their bets
-    @players.each do |k, p| 
-
-      p.cards = [get_card, get_card]
-
-      puts "Player #{k}, please enter your bet"
-      p.bet = gets.to_i
-    end
-    
-    print_game()
-    
-    game_loop()
-    
-  end
-  
-  def init_game()
-    
     # Waiting to enter the number of players
     puts "Please enter the number of players for the game:"
-    n = gets.to_i
+    n = 0
     
-    # Initialize the players (there is atleast one player - sorry hardcoded!)
-    for i in 1..n
-      @players[i] = Player.new(1000, Array.new, false, i)
+    while n <= 0
+      puts "Enter a number greater than 0"
+      n = gets.to_i
     end
     
-    # Initializing the card deck
-    deck_size = @num_decks * 4
-    deck_size.times {@cards += SUITE}
-    
+    # Initialize the players (there is atleast one player - sorry hardcoded!)
+    for i in 1...n+1
+      @players[i] = Player.new(1000, Array.new, i)
+    end
   end
   
   # Gets a random card from the deck
